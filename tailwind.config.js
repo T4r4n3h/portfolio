3225bd836076e00f039b3f5a853070b741e48e0c
Tara Saleh <taraneh.saleh@gmail.com>
2022-01-24 08:00:00 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors:{
+      peach: '#FEC5B9',
+      tangerine:'#FEC5B9',
+      bgdrk:'#39354D',
+      eggplant:'#645E80',
+      lavender:'#8A70FF',
+      iris: '#DEC4F4',
+      jasmine:'#E8D9F4',
+      
+    },
+    extend: {},
+  },
+  plugins: [],
+}
