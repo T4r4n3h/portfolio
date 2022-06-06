f85e00d452cce719d27110ff7cab256087563c3a
Taraneh Saleh <domina@Taranehs-MacBook-Pro.local>
2022-06-06 07:55:42 -0400

    change contact messaging

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
           </p>
           <p className=" text-jasmine font-light leading-8  text-base md:text-2xl md:leading-10 mt-8">
             <span className="text-iris font-medium ">&#62; </span>
-            I would love to hear from you and my inbox is always open. Whether you have a question or just want to say hi, I will get back to you! 
+            I would love to hear from you and my inbox is always open. Whether you have a question or just want to say hi, I look forward to hearing from you! 
           </p>
 
           <button className="border-[3px] border-tangerine text-peach text-xl p-4 mt-10">
